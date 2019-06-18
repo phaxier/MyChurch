ef4f653737da5901a33d2619765d51e5adcb9e3d
Noel Bor <phaxier@gmail.com>
2019-06-18 13:15:11 +0300

    added update on intenet and toast to the DailyActivity.java

--- app/src/main/java/com/mychurch/DailyActivity.java
+++ app/src/main/java/com/mychurch/DailyActivity.java
@@ -2,24 +2,39 @@ package com.mychurch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DailyActivity extends AppCompatActivity {
     @BindView(R.id.locationTextView)
     TextView mLocationTextView;
     @BindView(R.id.listView)
     ListView mListView;
-    private String[] restaurants = new String[] {"Our daily reading today Comes from the Book of Mark"};
-    private String[] cuisines = new String[]{"Add Your prayers"};
+    private String[] readings = new String[] {"Our daily reading today Comes from the Book of Mark"};
+    private String[] prayers = new String[]{"Add Your prayers"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily);
+        ButterKnife.bind(this);
 
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        MyChurchArrayAdapter adapter = new MyChurchArrayAdapter(this, android.R.layout.simple_list_item_1, readings, prayers);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener((adapterView, view, i, l) -> {
+            String readings = ((TextView)view).getText().toString();
+            Toast.makeText(DailyActivity.this, readings, Toast.LENGTH_LONG).show();
+        });
+        mLocationTextView.setText("Here are the daily readings: " + location);
     }
 }
