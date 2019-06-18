574c2ec8218a1444ffa88c4924d04ec6ae3a958c
Noel Bor <phaxier@gmail.com>
2019-06-18 13:23:54 +0300

    created MyChurchAdapter.java and updated it

---
+++ app/src/main/java/com/mychurch/MyChurchArrayAdapter.java
@@ -0,0 +1,34 @@
+package com.mychurch;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+
+public class MyChurchArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mReadings;
+    private String[] mPrayers;
+
+    public MyChurchArrayAdapter (Context mContext, int resource, String[] mReadings, String[] mPrayers){
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mPrayers = mPrayers;
+        this.mReadings = mReadings;
+    }
+
+
+     @Override
+    public int getCount() {
+        return mReadings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        String readings = mReadings[getPosition(mReadings)];
+        String prayers = mPrayers[getPosition(mPrayers)];
+        return String.format("%s \n Serves great: %s", readings, prayers);
+
+    }
+
+
+}
