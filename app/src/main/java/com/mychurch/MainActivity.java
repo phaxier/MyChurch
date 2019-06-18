43f6cd645b8df2a5351fa4b2365260c4ecfdcc18
Noel Bor <phaxier@gmail.com>
2019-06-18 10:41:22 +0300

    made changes to MainActivity.java

--- app/src/main/java/com/mychurch/MainActivity.java
+++ app/src/main/java/com/mychurch/MainActivity.java
@@ -2,13 +2,42 @@ package com.mychurch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.DatagramSocket;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.loginButton); Button mLoginButton;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+    @BindView(R.id.locationTextView)  EditText mLocationEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DailyActivity.class);
+                String location = mLocationEditText.getText().toString();
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
     }
 }
