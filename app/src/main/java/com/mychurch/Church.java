a509c66b670c9d0d12dddd703c84ba8d8b3d1f8e
Noel Bor <phaxier@gmail.com>
2019-07-08 09:31:43 +0300

    created Church.java file and inputted logic and made amendments where necessary.

---
+++ app/src/main/java/com/mychurch/Church.java
@@ -0,0 +1,30 @@
+package com.mychurch;
+
+public class Church {
+
+    private String mHolyBook;
+    private String mBookChapter;
+    private String mBookVerseFrom;
+    private String mBookVerseTo;
+
+public Church(String holyBook, String bookChapter, String bookVerseFrom, String bookVerseTo)
+    {
+        String mHolyBook = holyBook;
+        this.mBookChapter = bookChapter;
+        this.mBookVerseFrom = bookVerseFrom;
+        this.mBookVerseTo = bookVerseTo;
+
+    }
+    public String getHolyBook(){
+        return mHolyBook;
+    }
+    public String getBookChapter(){
+        return mBookChapter;
+    }
+    public String getBookVerseFrom(){
+        return mBookVerseFrom;
+    }
+    public String getBookVerseTo(){
+        return mBookVerseTo;
+    }
+}
