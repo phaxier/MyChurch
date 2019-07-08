8f4f04e68bffeb09f668028d337e292060615ffa
Noel Bor <phaxier@gmail.com>
2019-07-08 09:34:34 +0300

    created Constants.java file and inputted logic and made amendments where necessary.

---
+++ app/src/main/java/com/mychurch/Constants.java
@@ -0,0 +1,13 @@
+package com.mychurch;
+
+public class Constants {
+    public final static String BIBLE_TOKEN = BuildConfig.BIBLE_TOKEN;
+
+    public static final String BIBLE_Base_URL = "https://ajith-holy-bible.p.rapidapi.com/GetVerseOfaChapter";
+    public static final String BIBLE_BOOK_QUERY_PARAMETER = "Book";
+    public static final String BIBLE_CHAPTER_QUERY_PARAMETER = "chapter";
+    public static final String BIBLE_VERSE_QUERY_PARAMETER = "VerseFrom";
+    public static final String BIBLE_VERSETO_QUERY_PARAMETER = "VerseTo";
+
+
+}
