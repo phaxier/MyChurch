67305fd40e25b9655aa5f38d81708e249557fd5f
Noel Bor <phaxier@gmail.com>
2019-07-08 09:33:21 +0300

    created ChurchArrayAdapter.java file and inputted logic and made amendments where necessary.

---
+++ app/src/main/java/com/mychurch/ChurchArrayAdapter.java
@@ -0,0 +1,34 @@
+package com.mychurch;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+
+
+public class ChurchArrayAdapter extends ArrayAdapter {
+
+    private Context mContext;
+    private String[] mDailies;
+    private String[] mPrayer;
+
+
+    public ChurchArrayAdapter(Context mContext,int resource, String[] mDailies, String[] mPrayer ){
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mDailies = mDailies;
+        this.mPrayer = mPrayer;
+
+    }
+    @Override
+    public Object getItem(int position) {
+        String dailies = mDailies[position];
+//        String prayer = mPrayer[position];
+
+        return String.format("%s \nServes great:", dailies);
+    }
+    @Override
+    public int getCount() {
+        return mDailies.length;
+    }
+
+}
