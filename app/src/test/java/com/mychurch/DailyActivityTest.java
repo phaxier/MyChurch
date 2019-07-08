6695cc67d2faf7e11b74337a74d3a109d6a53a6e
Noel Bor <phaxier@gmail.com>
2019-07-08 09:56:15 +0300

    created DailyActivityTest.java added some logic to it.

---
+++ app/src/test/java/com/mychurch/DailyActivityTest.java
@@ -0,0 +1,35 @@
+package com.mychurch;
+
+
+import android.widget.ListView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
+
+@RunWith(RobolectricTestRunner.class)
+
+public class DailyActivityTest {
+
+    private DailyActivity activity;
+    private ListView mReadingsListView;
+
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(DailyActivity.class);
+        mReadingsListView = (ListView) activity.findViewById(R.id.listView);
+    }
+
+    @Test
+    public void restaurantListViewPopulates() {
+        assertNotNull(mReadingsListView.getAdapter());
+        assertEquals(mReadingsListView.getAdapter().getCount(), 15);
+    }
+
+}
